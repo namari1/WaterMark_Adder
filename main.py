d78e8ed7e533ae9b2033fc11dbdc8f2786cbae70
namari1 <noornamari@gmail.com>
2023-08-15 19:46:16 -0400

    Updated fonts and image resizer; added code to catch bugs and display messagebox warnings

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 
 global img, filename
 
-font_options = ["Arial", "Bahnschrift", "Calibri", "Cambria", "Candara", "Comic Sans MS", "Consolas", "Constantia", "Corbel", "Courier New",
-                "Ebrima", "Franklin Gothic Medium", "Gabriola", "Gadugi", "Georgia", "Impact", "Ink Free", "Javanese Text", "Leelawadee UI",
-                "Lucida Console", "Malgun Gothic", "Mongolian Baiti", "MV Boli", "Myanmar Text", "Nirmala UI", "Palatino Linotype",
-                "Segoe UI", "Sitka Text", "Sylfaen", "Symbol", "Tahoma", "Times New Roman", "Trebuchet MS",
-                "Verdana", "Webdings"]
+font_options = ["Arial", "Bahnschrift", "Calibri", "Cambria", "Comic Sans MS", "Consolas", "Constantia", "Corbel",
+                "Courier New",
+                "Ebrima", "Franklin Gothic Medium", "Gabriola", "Gadugi", "Georgia", "Impact", "Ink Free",
+                "Javanese Text", "Malgun Gothic", "Mongolian Baiti", "MV Boli", "Myanmar Text", "Nirmala UI",
+                "Palatino Linotype",
+                "Segoe UI", "Sitka Text", "Sylfaen", "Tahoma", "Times New Roman", "Trebuchet MS",
+                "Verdana"]
 
-font_dict = {"Arial": "arial.ttf", "Bahnschrift": "bahnschrift.ttf", "Calibri": "calibri.ttf", "Cambria": "cambriab.ttf", "Candara": "candara.ttf", "Comic Sans MS": "comic.ttf", "Consolas": "consola.ttf", "Constantia": "constan.ttf", "Corbel": "corbel.ttf", "Courier New": "cour.ttf",
-                "Ebrima": "ebrima.ttf", "Franklin Gothic Medium": "framd.ttf", "Gabriola": "Gabriola.ttf", "Gadugi": "gadugi.ttf", "Georgia": "georgia.ttf", "Impact": "impact.ttf", "Ink Free": "Inkfree.ttf", "Javanese Text": "javatext.ttf", "Leelawadee UI": "leelawui.ttf",
-                "Lucida Console": "lucon.TTF", "Malgun Gothic": "malgun.ttf", "Mongolian Baiti": "monbaiti.ttf", "MV Boli": "mvboli.ttf", "Myanmar Text": "mmrtext.ttf", "Nirmala UI": "Nirmala.ttf", "Palatino Linotype": "pala.ttf",
-                "Segoe UI": "segoeui.ttf", "Sitka Text": "SitkaVF.ttf", "Sylfaen": "sylfaen.ttf", "Symbol": "symbol.ttf", "Tahoma": "tahoma.ttf", "Times New Roman": "times.ttf", "Trebuchet MS": "trebuc.ttf",
-                "Verdana": "verdana.ttf", "Webdings": "webdings.ttf"}
+font_dict = {"Arial": "arial.ttf", "Bahnschrift": "bahnschrift.ttf", "Calibri": "calibri.ttf",
+             "Cambria": "cambriab.ttf", "Comic Sans MS": "comic.ttf", "Consolas": "consola.ttf",
+             "Constantia": "constan.ttf", "Corbel": "corbel.ttf", "Courier New": "cour.ttf",
+             "Ebrima": "ebrima.ttf", "Franklin Gothic Medium": "framd.ttf", "Gabriola": "Gabriola.ttf",
+             "Gadugi": "gadugi.ttf", "Georgia": "georgia.ttf", "Impact": "impact.ttf", "Ink Free": "Inkfree.ttf",
+             "Javanese Text": "javatext.ttf", "Malgun Gothic": "malgun.ttf", "Mongolian Baiti": "monbaiti.ttf",
+             "MV Boli": "mvboli.ttf", "Myanmar Text": "mmrtext.ttf", "Nirmala UI": "Nirmala.ttf",
+             "Palatino Linotype": "pala.ttf",
+             "Segoe UI": "segoeui.ttf", "Sitka Text": "SitkaVF.ttf", "Sylfaen": "sylfaen.ttf", "Tahoma": "tahoma.ttf",
+             "Times New Roman": "times.ttf", "Trebuchet MS": "trebuc.ttf",
+             "Verdana": "verdana.ttf"}
 
 
 def change_size(image):
     width = image.width
     height = image.height
-    if width and height > 3000:
-        width = int(width / 15)
-        height = int(height / 15)
-    elif width and height > 1200:
+    if width > 2000 or height > 2000:
+        width = int(width / 10)
+        height = int(height / 10)
+    elif width > 1200 or height > 1200:
         width = int(width / 4)
         height = int(height / 4)
-    elif width and height > 400:
+    elif width > 400 or height > 400:
         width = int(width / 2)
         height = int(height / 2)
     return width, height
@@ -34,44 +42,82 @@ def change_size(image):
 
 def open_img():
     global img, filename
-    filename = filedialog.askopenfilename()
-    img = Image.open(filename)
-    width, height = change_size(img)
-    img = img.resize((width, height), Image.BICUBIC)
-    image_size = Label(text=f"Image Width: {width}\nImage Height: {height}")
-    image_size.grid(row=10, column=0)
-    img = ImageTk.PhotoImage(img)
-    panel.config(image=img)
+    try:
+        filename = filedialog.askopenfilename()
+        img = Image.open(filename)
+        width, height = change_size(img)
+        img = img.resize((width, height), Image.BICUBIC)
+        image_size = Label(text=f"Image Width: {width}\nImage Height: {height}")
+        image_size.grid(row=10, column=0)
+        img = ImageTk.PhotoImage(img)
+        panel.config(image=img)
+    except NameError:
+        messagebox.showerror(title="Error", message="Please select a .png or .jpg file to add a watermark to.")
 
 
 def add_watermark():
     global img, filename
-    img = Image.open(filename)
+
+    try:
+        img = Image.open(filename)
+    except NameError:
+        messagebox.showerror(title="Error", message="Please select a .png or .jpg file to add a watermark to.")
+
     img.putalpha(255)
     width, height = change_size(img)
     img = img.resize((width, height), Image.BICUBIC)
-    txt_img = Image.new("RGBA", img.size, (255,255,255,0))
+
+    txt_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
     drawing = ImageDraw.Draw(txt_img)
-    pos = (float(x_pos.get()), float(y_pos.get()))
+
+    if len(x_pos.get()) == 0 or len(y_pos.get()) == 0:
+        messagebox.showerror(title="Error", message="X and Y positions must be chosen to add watermark.")
+    else:
+        pos = (float(x_pos.get()), float(y_pos.get()))
+
+    # Color
     red = int(red_scale.get())
     green = int(green_scale.get())
     blue = int(blue_scale.get())
     transparency = int(opacity_scale.get())
     font_color = (red, green, blue, transparency)
+
+    # Text
     text = text_entry.get()
+    if len(text) == 0:
+        messagebox.showerror(title="Error", message="Text must be entered to add watermark.")
     text_size = int(size_scale.get())
-    font_chosen = font_dict[font_var.get()]
-    font = ImageFont.truetype(font_chosen, text_size, encoding="unic")
+
+    # Font
+    if len(font_var.get()) == 0:
+        messagebox.showerror(title="Error", message="Font must be chosen to add watermark.")
+    else:
+        font_chosen = font_dict[font_var.get()]
+        font = ImageFont.truetype(font_chosen, text_size, encoding="unic")
+
+    # Add Watermark
     drawing.text(pos, text, fill=font_color, font=font)
     rotation = angle_var.get()
     txt_img = txt_img.rotate(rotation)
-    composite = Image.alpha_composite(img, txt_img)
-    if "PNG" or "png" in filename:
-        composite.save(f"{filename}.png")
-    else:
-        composite.save(f"{filename}.jpg")
-    img = ImageTk.PhotoImage(composite)
-    panel.config(image=img)
+
+    try:
+        composite = Image.alpha_composite(img, txt_img)
+        # Save image
+        filename_lower = filename.lower()
+        if "png" in filename_lower:
+            composite.save(f"{filename[:-4]}_watermarked.png")
+        elif "jpg" in filename_lower or "jpeg" in filename_lower:
+            composite = composite.convert("RGB")
+            composite.save(f"{filename[:-4]}_watermarked.jpg")
+        else:
+            composite.save(f"{filename[:-4]}_watermarked.png")
+        # Display image
+        img = ImageTk.PhotoImage(composite)
+        panel.config(image=img)
+    except ValueError:
+        messagebox.showerror(title="Error",
+                             message="Image you've selected cannot be watermarked, please select another one without "
+                                     "a transparent background.")
 
 
 window = Tk()
